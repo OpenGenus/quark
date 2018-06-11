c51f78bfb1ce53ff62fc70c1727a8f43637e0389
Stas Pochynok <staspochyn@gmail.com>
2018-06-11 16:12:16 -0400

    added different categories for images

--- code/images.js
+++ code/images.js
@@ -1,71 +1,95 @@
 var id = 0
-var dict = {1: "google.com",
-                2: "yahoo.com",
-                3: "facebook.com",
-                4: "google.com",
-                5: "website.com"}
+var category = getQueryVariable("category")
 
 function show_image(num) {
-    var img = document.createElement("img");
-    img.src = "images/" + num + ".jpg";
-    img.onerror = function() {
-        img.src = "images/" + num + ".png";
-        img.onerror = function() {
-            img.src = "images/" + num + ".gif";
-        };
-    };
-    img.style = "text-align:center; vertical-align:middle; max-width:100%;  max-height:100%;"
-
-    var outerDiv = document.getElementById("outer");
-    var innerDiv = document.getElementById("inner");
-    innerDiv.parentNode.removeChild(innerDiv);
-
-    var div = document.createElement("div");
-    div.id = "inner"
-    outerDiv.appendChild(div);
-    var theDiv = document.getElementById("inner");
-    theDiv.appendChild(img);
-    document.getElementById("credits").innerHTML = "The credit of the image goes to " + dict[num] + " !";
-
+    $.getJSON("images.json", function(json) {
+        var dict = json.category.filter(function (entry) {
+            return entry.name === category;
+        });
+        var img = document.createElement("img");
+        img.src = "images/" + category + "/" + dict[0].specific_source[num]["file"];
+        img.style = "text-align:center; vertical-align:middle; max-width:100%;  max-height:100%;"
+    
+        var outerDiv = document.getElementById("outer");
+        var innerDiv = document.getElementById("inner");
+        innerDiv.parentNode.removeChild(innerDiv);
+    
+        var div = document.createElement("div");
+        div.id = "inner"
+        outerDiv.appendChild(div);
+        var theDiv = document.getElementById("inner");
+        theDiv.appendChild(img);
+        var credits = "";
+        if (dict[0].specific_source[num]["source"] == "" || dict[0].specific_source[num]["source"] == undefined){
+            credits = dict[0].source;
+        } else {
+            credits = dict[0].specific_source[num]["source"]
+        }
+        document.getElementById("credits").innerHTML = "The credit of the image goes to " + credits + " !";
+    });
 }
 function increase(){
-    id += 1;
-    if (id == Object.keys(dict).length - 1) {
-        document.getElementById("next").disabled = true;
-    }
-    if (id > 0) {
-        document.getElementById("previous").disabled = false;
-    }
 
-    show_image(id + 1);
+    $.getJSON("images.json", function(json) {
+        var dict = json.category.filter(function (entry) {
+            return entry.name === category;
+        });
+        id += 1;
+        if (id == Object.keys( dict[0].specific_source).length - 1) {
+            document.getElementById("next").disabled = true;
+        }
+        if (id > 0) {
+            document.getElementById("previous").disabled = false;
+        }
 
+        show_image(id);
+        });
 }
 
 function decrease(){
-    id -= 1;
-    if (id == 0) {
-        document.getElementById("previous").disabled = true;
-    }
-    if (id < Object.keys(dict).length - 1) {
-        document.getElementById("next").disabled = false;
-    }
-    show_image(id + 1);
+    $.getJSON("images.json", function(json) {
+        var dict = json.category.filter(function (entry) {
+            return entry.name === category;
+        });
+        id -= 1;
+        if (id == 0) {
+            document.getElementById("previous").disabled = true;
+        }
+        if (id < Object.keys( dict[0].specific_source).length - 1) {
+            document.getElementById("next").disabled = false;
+        }
+        show_image(id);
+    });
 }
 
 function random(){
-    id = Math.floor((Math.random() * Object.keys(dict).length) + 0);
-    if (id == 0) {
-        document.getElementById("previous").disabled = true;
-    } else {
-        document.getElementById("previous").disabled = false;
-    }
-    if (id == Object.keys(dict).length - 1) {
-        document.getElementById("next").disabled = true;
-    } else {
-        document.getElementById("next").disabled = false;
-    }
+    $.getJSON("images.json", function(json) {
+        var dict = json.category.filter(function (entry) {
+            return entry.name === category;
+        });
+        id = Math.floor((Math.random() * Object.keys( dict[0].specific_source).length));
+        if (id == 0) {
+            document.getElementById("previous").disabled = true;
+        } else {
+            document.getElementById("previous").disabled = false;
+        }
+        if (id == Object.keys( dict[0].specific_source).length - 1) {
+            document.getElementById("next").disabled = true;
+        } else {
+            document.getElementById("next").disabled = false;
+        }
 
-    show_image(id + 1);
+        show_image(id);
+    });
+}
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+            var pair = vars[i].split("=");
+            if(pair[0] == variable){return pair[1];}
+    }
+    return(false);
 }
 
 document.getElementById('previous').addEventListener('click',decrease);
@@ -73,16 +97,6 @@ document.getElementById('next').addEventListener('click',increase);
 document.getElementById('random').addEventListener('click',random);
 
 $( document ).ready(function() {
-    show_image(id + 1);
+    show_image(id);
     document.getElementById("previous").disabled = true;
 });
-
-function tryImages(img, src) {
-    img.src = "images/" + name + ".jpg";
-    img.onerror = function() {
-        img.src = "img/" + name + ".png";
-        img.onerror = function() {
-            img.src = "img/" + name + ".gif";
-        };
-    };
-}
