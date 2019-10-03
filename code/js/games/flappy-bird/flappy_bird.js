7a4d07cea0646316ed9b7152cc20df5c830ac212
aryabharat <arya.bharat@rocketmail.com>
2019-10-03 21:18:31 +0530

    Different Files are made for each object

--- code/js/games/flappy-bird/flappy_bird.js
+++ code/js/games/flappy-bird/flappy_bird.js
@@ -117,81 +117,6 @@ const fg = {
     }
 }
 
-// BIRD
-const bird = {
-    animation : [
-        {sX: 276, sY : 112},
-        {sX: 276, sY : 139},
-        {sX: 276, sY : 164},
-        {sX: 276, sY : 139}
-    ],
-    x : 50,
-    y : 150,
-    w : 34,
-    h : 26,
-    
-    radius : 12,
-    
-    frame : 0,
-    
-    gravity : 0.25,
-    jump : 4.6,
-    speed : 0,
-    rotation : 0,
-    
-    draw : function(){
-        let bird = this.animation[this.frame];
-        
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(this.rotation);
-        ctx.drawImage(sprite, bird.sX, bird.sY, this.w, this.h,- this.w/2, - this.h/2, this.w, this.h);
-        
-        ctx.restore();
-    },
-    
-    flap : function(){
-        this.speed = - this.jump;
-    },
-    
-    update: function(){
-        // IF THE GAME STATE IS GET READY STATE, THE BIRD MUST FLAP SLOWLY
-        this.period = state.current == state.getReady ? 10 : 5;
-        // WE INCREMENT THE FRAME BY 1, EACH PERIOD
-        this.frame += frames%this.period == 0 ? 1 : 0;
-        // FRAME GOES FROM 0 To 4, THEN AGAIN TO 0
-        this.frame = this.frame%this.animation.length;
-        
-        if(state.current == state.getReady){
-            this.y = 150; // RESET POSITION OF THE BIRD AFTER GAME OVER
-            this.rotation = 0 * DEGREE;
-        }else{
-            this.speed += this.gravity;
-            this.y += this.speed;
-            
-            if(this.y + this.h/2 >= cvs.height - fg.h){
-                this.y = cvs.height - fg.h - this.h/2;
-                if(state.current == state.game){
-                    state.current = state.over;
-                    DIE.play();
-                }
-            }
-            
-            // IF THE SPEED IS GREATER THAN THE JUMP MEANS THE BIRD IS FALLING DOWN
-            if(this.speed >= this.jump){
-                this.rotation = 90 * DEGREE;
-                this.frame = 1;
-            }else{
-                this.rotation = -25 * DEGREE;
-            }
-        }
-        
-    },
-    speedReset : function(){
-        this.speed = 0;
-    }
-}
-
 // GET READY MESSAGE
 const getReady = {
     sX : 0,
@@ -226,117 +151,6 @@ const gameOver = {
     
 }
 
-// PIPES
-const pipes = {
-    position : [],
-    
-    top : {
-        sX : 553,
-        sY : 0
-    },
-    bottom:{
-        sX : 502,
-        sY : 0
-    },
-    
-    w : 53,
-    h : 400,
-    gap : 85,
-    maxYPos : -150,
-    dx : 2,
-    
-    draw : function(){
-        for(let i  = 0; i < this.position.length; i++){
-            let p = this.position[i];
-            
-            let topYPos = p.y;
-            let bottomYPos = p.y + this.h + this.gap;
-            
-            // top pipe
-            ctx.drawImage(sprite, this.top.sX, this.top.sY, this.w, this.h, p.x, topYPos, this.w, this.h);  
-            
-            // bottom pipe
-            ctx.drawImage(sprite, this.bottom.sX, this.bottom.sY, this.w, this.h, p.x, bottomYPos, this.w, this.h);  
-        }
-    },
-    
-    update: function(){
-        if(state.current !== state.game) return;
-        
-        if(frames%100 == 0){
-            this.position.push({
-                x : cvs.width,
-                y : this.maxYPos * ( Math.random() + 1)
-            });
-        }
-        for(let i = 0; i < this.position.length; i++){
-            let p = this.position[i];
-            
-            let bottomPipeYPos = p.y + this.h + this.gap;
-            
-            // COLLISION DETECTION
-            // TOP PIPE
-            if(bird.x + bird.radius > p.x && bird.x - bird.radius < p.x + this.w && bird.y + bird.radius > p.y && bird.y - bird.radius < p.y + this.h){
-                state.current = state.over;
-                HIT.play();
-            }
-            // BOTTOM PIPE
-            if(bird.x + bird.radius > p.x && bird.x - bird.radius < p.x + this.w && bird.y + bird.radius > bottomPipeYPos && bird.y - bird.radius < bottomPipeYPos + this.h){
-                state.current = state.over;
-                HIT.play();
-            }
-            
-            // MOVE THE PIPES TO THE LEFT
-            p.x -= this.dx;
-            
-            // if the pipes go beyond canvas, we delete them from the array
-            if(p.x + this.w <= 0){
-                this.position.shift();
-                score.value += 1;
-                SCORE_S.play();
-                score.best = Math.max(score.value, score.best);
-                localStorage.setItem("best", score.best);
-            }
-        }
-    },
-    
-    reset : function(){
-        this.position = [];
-    }
-    
-}
-
-// SCORE
-const score= {
-    best : parseInt(localStorage.getItem("best")) || 0,
-    value : 0,
-    
-    draw : function(){
-        ctx.fillStyle = "#FFF";
-        ctx.strokeStyle = "#000";
-        
-        if(state.current == state.game){
-            ctx.lineWidth = 2;
-            ctx.font = "35px Teko";
-            ctx.fillText(this.value, cvs.width/2, 50);
-            ctx.strokeText(this.value, cvs.width/2, 50);
-            
-        }else if(state.current == state.over){
-            // SCORE VALUE
-            ctx.font = "25px Teko";
-            ctx.fillText(this.value, 625, 186);
-            ctx.strokeText(this.value, 625, 186);
-            // BEST SCORE
-            ctx.fillText(this.best, 625, 228);
-            ctx.strokeText(this.best, 625, 228);
-        }
-    },
-    
-    reset : function(){
-        this.value = 0;
-    }
-}
-
 // DRAW
 function draw(){
     ctx.fillStyle = "#70c5ce";
@@ -363,7 +177,6 @@ function loop(){
     update();
     draw();
     frames++;
-    
     requestAnimationFrame(loop);
 }
 loop();
---
+++ code/js/games/flappy-bird/pipe.js
@@ -0,0 +1,78 @@
+const pipes = {
+    position : [],
+    
+    top : {
+        sX : 553,
+        sY : 0
+    },
+    bottom:{
+        sX : 502,
+        sY : 0
+    },
+    
+    w : 53,
+    h : 400,
+    gap : 85,
+    maxYPos : -150,
+    dx : 2,
+    
+    draw : function(){
+        for(let i  = 0; i < this.position.length; i++){
+            let p = this.position[i];
+            
+            let topYPos = p.y;
+            let bottomYPos = p.y + this.h + this.gap;
+            
+            // top pipe
+            ctx.drawImage(sprite, this.top.sX, this.top.sY, this.w, this.h, p.x, topYPos, this.w, this.h);  
+            
+            // bottom pipe
+            ctx.drawImage(sprite, this.bottom.sX, this.bottom.sY, this.w, this.h, p.x, bottomYPos, this.w, this.h);  
+        }
+    },
+    
+    update: function(){
+        if(state.current !== state.game) return;
+        
+        if(frames%100 == 0){
+            this.position.push({
+                x : cvs.width,
+                y : this.maxYPos * ( Math.random() + 1)
+            });
+        }
+        for(let i = 0; i < this.position.length; i++){
+            let p = this.position[i];
+            
+            let bottomPipeYPos = p.y + this.h + this.gap;
+            
+            // COLLISION DETECTION
+            // TOP PIPE
+            if(bird.x + bird.radius > p.x && bird.x - bird.radius < p.x + this.w && bird.y + bird.radius > p.y && bird.y - bird.radius < p.y + this.h){
+                state.current = state.over;
+                HIT.play();
+            }
+            // BOTTOM PIPE
+            if(bird.x + bird.radius > p.x && bird.x - bird.radius < p.x + this.w && bird.y + bird.radius > bottomPipeYPos && bird.y - bird.radius < bottomPipeYPos + this.h){
+                state.current = state.over;
+                HIT.play();
+            }
+            
+            // MOVE THE PIPES TO THE LEFT
+            p.x -= this.dx;
+            
+            // if the pipes go beyond canvas, we delete them from the array
+            if(p.x + this.w <= 0){
+                this.position.shift();
+                score.value += 1;
+                SCORE_S.play();
+                score.best = Math.max(score.value, score.best);
+                localStorage.setItem("best", score.best);
+            }
+        }
+    },
+    
+    reset : function(){
+        this.position = [];
+    }
+    
+}
